43d3bcdba05656131f186a20681f8858d8feabeb
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2017-06-30 20:01:06 -0300

    Partial commit

---
+++ src/main/java/com/alexandreesl/handson/ApacheCamelHandsonApp.java
@@ -0,0 +1,17 @@
+package com.alexandreesl.handson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by alexandrelourenco on 28/06/17.
+ */
+
+@SpringBootApplication
+public class ApacheCamelHandsonApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheCamelHandsonApp.class, args);
+    }
+
+}
