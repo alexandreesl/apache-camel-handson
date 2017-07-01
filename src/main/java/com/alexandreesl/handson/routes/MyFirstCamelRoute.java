0e3d66d337b7dc3d568840f3cfe47ec64fd99ea9
Alexandre Eleutério Santos Lourenço <alexandreesl@gmail.com>
2017-07-01 14:32:48 -0300

    Partial commit

--- src/main/java/com/alexandreesl/handson/routes/MyFirstCamelRoute.java
+++ src/main/java/com/alexandreesl/handson/routes/MyFirstCamelRoute.java
@@ -16,7 +16,12 @@ public class MyFirstCamelRoute extends SpringRouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("file:/Users/alexandrelourenco/Documents/apachecamelhandson?delay=1000&charset=utf-8&delete=true")
+        onException(Exception.class)
+                .handled(false)
+                .log(LoggingLevel.ERROR, "An Error processing the file!")
+                .to("smtps://smtp.gmail.com:465?password=xxxxxxxxxxxxxxx&username=alexandreesl@gmail.com&subject=A error has occurred!");
+
+        from("file:/Users/alexandrelourenco/Documents/apachecamelhandson?delay=1000&charset=utf-8&delete=true&moveFailed=.error")
                 .setHeader("CamelAwsS3Key", header("CamelFileName"))
                 .to("aws-s3:arn:aws:s3:::apache-camel-handson?amazonS3Client=#s3Client")
                 .convertBodyTo(String.class)
